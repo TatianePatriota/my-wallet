601ba8a1cba710eec5d0b4dd98b920c44076a7d8
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-26 14:12:36 -0300

    add cor

--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,25 @@
 export const themes = {
-    light: {
-        font: {
-            size: {
-                small: 13,
-                default: 16
-            }
-        },
-        gutter: {
-            default: 24
-        },
-        colors: {
-            black: '#222222',
-            gray: '#AEAFAE',
-            grayDark: '#CBCBDD',
-            background: '#F9FAFC',
-            backgroundDark: '#E9EAF2',
-            primary: '#031A6E',
-            secondary: '#031A6E',
-            red: '#E6492D',
-            green: '#34AA44'  
-        }
-    }
-}
+  light: {
+    font: {
+      size: {
+        small: 13,
+        default: 16,
+      },
+    },
+    gutter: {
+      default: 24,
+    },
+    colors: {
+      black: "#222222",
+      gray: "#AEAFAE",
+      grayDark: "#CBCBDD",
+      background: "#F9FAFC",
+      backgroundDark: "#E9EAF2",
+      primary: "#031A6E",
+      secondary: "#031A6E",
+      red: "#E6492D",
+      green: "#34AA44",
+      lightGray: "#EFEFEF",
+    },
+  },
+};
