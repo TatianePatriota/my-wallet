b73f1b8dc707c00bd02917371bda8bdb6f2b1c89
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-27 06:47:00 -0300

    add flex

--- src/components/home-resume.styled.js
+++ src/components/home-resume.styled.js
@@ -10,6 +10,7 @@ export const StyledHomeResumeHeaderTitle = styled.p`
   color: ${({ theme }) => theme.colors.gray};
   font-size: ${({ theme }) => theme.font.size.small}px;
   line-height: 1.4;
+  flex: 1;
 
   & strong {
     color: ${({ theme }) => theme.colors.black};
@@ -19,9 +20,12 @@ export const StyledHomeResumeHeaderTitle = styled.p`
 
 export const StyledHomeRsumeBalance = styled.div`
   background-color: ${({ theme }) => theme.colors.primary};
-  padding: ${({ theme }) => theme.gutter.large}px ${({ theme }) => theme.gutter.small}px ${({ theme }) => theme.gutter.small}px;
-  margin: ${({ theme }) => theme.gutter.large}px ${({ theme }) => theme.gutter.default}px;
+  padding: ${({ theme }) => theme.gutter.large}px
+    ${({ theme }) => theme.gutter.small}px
+    ${({ theme }) => theme.gutter.small}px;
+  margin: ${({ theme }) => theme.gutter.large}px
+    ${({ theme }) => theme.gutter.default}px;
   border-radius: 8px;
   box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.05);
   color: ${({ theme }) => theme.colors.white};
-`
+`;
