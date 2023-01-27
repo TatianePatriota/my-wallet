6cb8a81d5f33a14c11c5bd1d6ddd648db0f4110e
Lázaro Alvarenga <lazaroalvarenga@email.com>
2023-01-27 17:49:44 -0300

    add sample service storage & manifest

---
+++ src/services/storage-service.js
@@ -0,0 +1,67 @@
+function registerId() {
+  return String(Date.now().toString(32) + Math.random().toString(16)).replace(
+    /\./g,
+    ""
+  );
+}
+
+export function localStorageService() {
+  function get(itemName) {
+    const item = localStorage.getItem(itemName);
+    const numPatt = new RegExp(/^\d+$/);
+    const jsonPatt = new RegExp(/[\[\{].*[\}\]]/);
+
+    if (item) {
+      if (jsonPatt.test(item)) {
+        return JSON.parse(item);
+      } else if (numPatt.test(item)) {
+        return parseFloat(item);
+      } else {
+        return item;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  function set(itemName, item) {
+    if (typeof item !== "object") {
+      throw new Error("Saved data should be a valid object");
+    }
+    localStorage.setItem(
+      itemName,
+      JSON.stringify({ ...item, id: registerId() })
+    );
+  }
+
+  return {
+    get,
+    set,
+    increment(itemName, value) {
+      const arr = get(itemName);
+
+      if (arr === null) {
+        throw new Error(
+          "List should be initialized before increment a new item"
+        );
+      }
+
+      return set(itemName, [...arr, value]);
+    },
+    decrement(itemName, id) {
+      const arr = get(itemName);
+
+      if (arr === null) {
+        throw new Error(
+          "List should be initialized before increment a new item"
+        );
+      }
+
+      return set(itemName, [...arr.filter((t) => t.id !== id)]);
+    },
+
+    // remove(itemName) {
+    //   localStorage.removeItem(itemName);
+    // }
+  };
+}
