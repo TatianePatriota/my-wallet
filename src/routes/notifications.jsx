ad803dee5a9c3000a28fe8468046efa6509ca2a9
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-27 09:50:02 -0300

    editando componente

--- src/routes/notifications.jsx
+++ src/routes/notifications.jsx
@@ -1,6 +1,3 @@
-import { LayoutWithoutBottomNavigation } from "../layouts/without-bottom-navigation";
-import { Transactions } from "./transactions";
-
 export function Notifications() {
   return <h1>em constução</h1>;
 }
