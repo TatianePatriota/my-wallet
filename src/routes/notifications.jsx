1136444bc99dd74c2e086e5425f2964dd5534b81
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-27 09:36:49 -0300

    add componente de notificacoes

---
+++ src/routes/notifications.jsx
@@ -0,0 +1,6 @@
+import { LayoutWithoutBottomNavigation } from "../layouts/without-bottom-navigation";
+import { Transactions } from "./transactions";
+
+export function Notifications() {
+  return <h1>em constução</h1>;
+}
