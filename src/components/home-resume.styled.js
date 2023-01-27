ffc6d856e3d5427344dffeb5e529c5d680d61637
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-27 09:36:14 -0300

    editando estilos

--- src/components/home-resume.styled.js
+++ src/components/home-resume.styled.js
@@ -18,7 +18,7 @@ export const StyledHomeResumeHeaderTitle = styled.p`
   }
 `;
 
-export const StyledHomeRsumeBalance = styled.div`
+export const StyledHomeResumeBalance = styled.div`
   background-color: ${({ theme }) => theme.colors.primary};
   padding: ${({ theme }) => theme.gutter.large}px
     ${({ theme }) => theme.gutter.small}px
@@ -28,4 +28,35 @@ export const StyledHomeRsumeBalance = styled.div`
   border-radius: 8px;
   box-shadow: 0px 5px 30px rgba(0, 0, 0, 0.05);
   color: ${({ theme }) => theme.colors.white};
+  position: relative;
+
+  & h1 {
+    margin: 4px;
+    line-height: 1.5;
+    font-size: ${({ theme }) => theme.font.size.small * 2}px;
+  }
+
+  & h2 {
+    margin: 0;
+    font-weight: 600;
+    font-size: ${({ theme }) => theme.font.size.default}px;
+    line-height: 1.5;
+    margin-bottom: 50px;
+  }
+`;
+
+export const StyledHomeResumeIcon = styled.svg`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+export const StyledHomeResumeIconEllipse = styled.svg`
+  position: absolute;
+  top: 0;
+  right: 18px;
+  width: 100%;
+  height: 100%;
 `;
