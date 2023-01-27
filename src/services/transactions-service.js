49995aa45a6bdf6f7dea5d8669f8fb0592751f71
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-01-27 09:37:03 -0300

    formatando arquivo

--- src/services/transactions-service.js
+++ src/services/transactions-service.js
@@ -1,38 +1,42 @@
-const today = new Intl.DateTimeFormat('en-GB', { dateStyle: 'medium', timeZone: 'America/Sao_Paulo' }).format(new Date())
+import styled from "styled-components";
+
+const today = new Intl.DateTimeFormat("en-GB", {
+  dateStyle: "medium",
+  timeZone: "America/Sao_Paulo",
+}).format(new Date());
 
 const mockedTransactions = [
-    {
-        id: '1',
-        title: 'Dropbox',
-        description: 'Assinatura anual',
-        value: 144.00,
-        invoicedAt: today
-    },
-    {
-        id: '22',
-        title: 'Spotify',
-        description: 'Assinatura mensal',
-        value: 24.00,
-        invoicedAt: today
-    },
-    {
-        id: '53',
-        title: 'Freelancer',
-        description: 'Recebimento pontual',
-        value: 421.00,
-        receivedAt: today
-    },
-    
-]
+  {
+    id: "1",
+    title: "Dropbox",
+    description: "Assinatura anual",
+    value: 144.0,
+    invoicedAt: today,
+  },
+  {
+    id: "22",
+    title: "Spotify",
+    description: "Assinatura mensal",
+    value: 24.0,
+    invoicedAt: today,
+  },
+  {
+    id: "53",
+    title: "Freelancer",
+    description: "Recebimento pontual",
+    value: 421.0,
+    receivedAt: today,
+  },
+];
 
 export function getRecentTransactions() {
-    return mockedTransactions
+  return mockedTransactions;
 }
 
 export function getAllTransactions() {
-    return [...mockedTransactions, ...mockedTransactions, ...mockedTransactions]
+  return [...mockedTransactions, ...mockedTransactions, ...mockedTransactions];
 }
 
 export function getBalance() {
-    return 6420.00
+  return 6420.0;
 }
