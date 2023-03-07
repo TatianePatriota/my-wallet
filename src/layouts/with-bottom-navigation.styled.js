071407c2b7c9f5cb8b9b9cc2e94e640f103534ee
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-03-07 14:06:59 -0300

    formatando arquivo

--- src/layouts/with-bottom-navigation.styled.js
+++ src/layouts/with-bottom-navigation.styled.js
@@ -2,24 +2,25 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const StyledWithBottonNavigationContainer = styled.div`
-    flex-direction: column;
-    width: 100%;
-    display: flex;
-`
+  flex-direction: column;
+  width: 100%;
+  display: flex;
+`;
 
 export const StyledWithBottonNavigationContent = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
 
-    overflow-y: scroll;
+  overflow-y: scroll;
 
-    &::-webkit-scrollbar {
-  display: none;
-}
-`
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
 export const StyledBackAction = styled(NavLink)`
-  padding: ${({ theme }) => theme.gutter.default}px ${({ theme }) => theme.gutter.default}px 0;
+  padding: ${({ theme }) => theme.gutter.default}px
+    ${({ theme }) => theme.gutter.default}px 0;
   background-color: ${({ theme }) => theme.colors.background};
-`
+`;
