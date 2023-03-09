6367e404ebb2d4a73bf41c0f6f90de90b824e60e
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-03-09 15:27:09 -0300

    add componente header de transacoes

---
+++ src/components/header-transactions.jsx
@@ -0,0 +1,10 @@
+import { IconWave } from "./common-icons";
+
+export function HeaderTransactions({ title, description }) {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+}
