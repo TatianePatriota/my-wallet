3988fa4d258d6c8b635f5f8b62b48f591599b36e
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-03-13 20:26:57 -0300

    fix router

---
+++ src/router.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./routes/home";
+import { Profile } from "./routes/profile";
+import { Transactions } from "./routes/transactions";
+
+export default createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/relatorios",
+    element: <Transactions />,
+  },
+  {
+    path: "/perfil",
+    element: <Profile />,
+  },
+  {
+    path: "/todas-as-transacoes",
+    element: <Transactions />,
+  },
+]);
